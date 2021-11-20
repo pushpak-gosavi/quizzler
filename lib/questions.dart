2666283dbaf6f7a0b51a81815da280d69154c245
Pushpak Gosavi <gosavi.pushpak@evonix.co>
2021-11-20 16:15:36 +0530

    cart Constructor aaded

--- lib/questions.dart
+++ lib/questions.dart
@@ -1,8 +1,9 @@
 class Questions{
    late String question;
    late bool answer;
-  Questions({q:String, a:bool}){
-    question=q;
-    answer = a;
-  }
+  // Questions({q:String, a:bool}){
+  //   question=q;
+  //   answer = a;
+  // }
+Questions({required this.question, required this.answer}); /*** Another way of writing the Constructor #Dart Sugar ***/
 }
